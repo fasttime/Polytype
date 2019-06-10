0771eebee04dd5a3ed26018cce638c7563e46127
Francesco Trotta <>
2019-06-10 12:20:09 +0200

    Fixed a spec runner issue with Node.js 8

--- test/node-spec-runner.js
+++ test/node-spec-runner.js
@@ -8,6 +8,7 @@ const { fork } = require('child_process');
 
 const mochaPath = require.resolve('mocha/bin/_mocha');
 const [,, extname] = process.argv;
+const env = extname == null ? null : { extname };
 const execArgv = ['--no-warnings'];
 if (extname === '.mjs')
     execArgv.push('--experimental-modules');
@@ -16,7 +17,7 @@ fork
 (
     mochaPath,
     ['--color', '--require=test/spec-helper.js', 'test/init-spec.js', 'test/spec/**/*.spec.js'],
-    { env: { extname }, execArgv },
+    { env, execArgv },
 );
 childProcess.on
 (
